d96381e46f09576ff23fa72500aa4017a0b8accc
Ricardo Henrique Araújo Falcão <101195473+RicardoHAF@users.noreply.github.com>
2022-05-20 22:15:19 +0000

    Adicionei um modo de separação para organizar melhor no print e deixei mais enquadrado.

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 nome = input('Nome: ')
 idade = int(input('Idade: '))
-peso = str(input('Peso/Kg: '))
-print("Ola,{}, Seja bem vindo, voce tem {} anos e pesa {}kg".format(nome,idade,peso))
+peso = str(input('Peso: '))
+print('=-=-='*7)
+print('''Olá,{}
+Seja bem vindo.
+Você tem {} anos e pesa {}kg'''.format(nome,idade,peso))
+print("=-=-="*7)
